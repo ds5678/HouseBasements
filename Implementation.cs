1051ea1bc03045b527b1424acaf2a52a8a3e83f3
ds5678 <jrpritts7@gmail.com>
2021-04-13 03:57:52 -0400

    Add project files.

---
+++ Implementation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace HouseBasements
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+        }
+        internal static void Log(string message, params object[] parameters) => MelonLogger.Log(message, parameters);
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,66 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HouseBasements
+{
+    class Patches
+    {
+        /*[HarmonyPatch(typeof(LoadScene), "Start")]
+        internal class UnitySceneManager
+        {
+            public static void Postfix(LoadScene __instance)
+            {
+                Implementation.Log("LoadScene Start: '{0}'", __instance.name);
+                if(__instance.name == "BasementDoorExitCollider")
+                {
+                    GameObject obj = __instance.gameObject;
+                    obj.SetActive(true);
+                    obj.transform.position = new Vector3(2.2f, 1.333f, 3.2f);
+                }
+            }
+        }*/
+        /*[HarmonyPatch(typeof(EmptyScene), "LoadScene")]
+        internal class EmptyScene6
+        {
+            public static void Postfix(string scene)
+            {
+                Implementation.Log("Load "+scene);
+                if(scene == "CoastalHouseE")
+                {
+                    GameObject obj = GameObject.Find("Root/Design/Scripting/BasementDoorExitCollider");
+                    obj.SetActive(true);
+                    obj.transform.position = new Vector3(2.2f, 1.333f, 3.2f);
+                }
+            }
+        }*/
+        [HarmonyPatch(typeof(GameManager), "Awake")]
+        internal class EmptyScene6
+        {
+            public static void Postfix()
+            {
+                string scene = GameManager.m_ActiveScene;
+                Implementation.Log("Awake "+scene);
+                if (scene == "CoastalHouseE")
+                {
+                    GameObject obj = GameObject.Find("Root/Design/Scripting/BasementDoorExitCollider");
+                    LoadScene loadScene = obj?.GetComponent<LoadScene>();
+                    if (loadScene) loadScene.m_SceneCanBeInstanced = true;
+                    obj.SetActive(true);
+                    obj.transform.position = new Vector3(2.2f, 1.333f, 3.2f);
+                }
+                else if (scene == "CoastalHouseF")
+                {
+                    GameObject obj = GameObject.Find("Root/Design/Scripting/BasementDoorExitCollider");
+                    LoadScene loadScene = obj?.GetComponent<LoadScene>();
+                    if (loadScene) loadScene.m_SceneCanBeInstanced = true;
+                    obj.SetActive(true);
+                }
+            }
+        }
+    }
+}
