1dd00cfe50346960c889aa04ea0eb8f3d99cebd8
ds5678 <jrpritts7@gmail.com>
2021-05-27 14:58:20 -0400

    Mesh fix

--- src/Patches.cs
+++ src/Patches.cs
@@ -6,35 +6,35 @@ namespace HouseBasements
 	class Patches
 	{
 		[HarmonyPatch(typeof(GameManager), "Awake")]
-		internal class EmptyScene6
+		internal class GameManager_Awake
 		{
+			public const string PATH = "assets/custom/mods/housebasements/";
 			public static void Postfix()
 			{
 				string scene = GameManager.m_ActiveScene;
-				bool sceneIsRegion = GameManager.m_ActiveSceneIsRegion;
-				Implementation.Log("Awake {0} {1}", scene, sceneIsRegion);
+				
 				if (scene == "AshCanyonRegion")
 				{
 					//1
-					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset("assets/housebasementsashcanyon.prefab")).Cast<GameObject>();
+					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset(PATH + "housebasementsashcanyon.prefab")).Cast<GameObject>();
 					Settings.SetBasementVisibility();
 				}
 				else if (scene == "CoastalRegion")
 				{
 					//4
-					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset("assets/housebasementscoastalhighway.prefab")).Cast<GameObject>();
+					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset(PATH + "housebasementscoastalhighway.prefab")).Cast<GameObject>();
 					Settings.SetBasementVisibility();
 				}
 				else if (scene == "MountainTownRegion")
 				{
 					//6
-					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset("assets/housebasementsmountaintown.prefab")).Cast<GameObject>();
+					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset(PATH + "housebasementsmountaintown.prefab")).Cast<GameObject>();
 					Settings.SetBasementVisibility();
 				}
 				else if (scene == "RuralRegion")
 				{
 					//2
-					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset("assets/housebasementspleasantvalley.prefab")).Cast<GameObject>();
+					Settings.basements = GameObject.Instantiate(Implementation.assetBundle.LoadAsset(PATH + "housebasementspleasantvalley.prefab")).Cast<GameObject>();
 					Settings.SetBasementVisibility();
 				}
 			}
